4709a2117ccb3c495950187dcb663a4cb4d3a4f5
ji_tai <3433175957@qq.com>
2026-06-15 15:01:42 +0800

    学习使用文档加载器和文档分割器

--- 08_PyPDF文档加载器.py
+++ 08_PyPDF文档加载器.py
@@ -1,7 +1,10 @@
 from langchain_community.document_loaders import PyPDFLoader
 
 pypdf_loader = PyPDFLoader(
-    file_path="",
+    file_path="./data/pdf/game_development.pdf",
     mode="page",   #读取模式，如果是page则会根据文件的页数获得不同的document,single无论多少页都会获得一个document，默认是page模式
-    password="",   #如果文件是加密的，则需要密码参数
+    # password="",   #如果文件是加密的，则需要密码参数
 )
+
+pdf_document = pypdf_loader.load()#page模式生成的结果是一个列表
+print(pdf_document)
---
+++ 08_基础文档加载器.py
@@ -0,0 +1,27 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+text_loader = TextLoader(
+    file_path="./data/txt文本/mermaid.txt",
+    encoding="utf-8"
+)
+
+txt_document = text_loader.load()
+print(txt_document)
+print(len(txt_document))
+#文档加载器返回的数据类型是List[Document]类型
+#文本文档加载器只能返回一个Document类型的数据即list大小为1
+#为了方便使用langchain官方使用RecursiveCharacterTextSplitter递归字符文档分割器，可以按自然段落分割大型文档
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,#允许的每段最大的字数
+    chunk_overlap=100,#两端之间允许重叠的字数
+    separators=["\n", "。", "？", "！", ".", "?", "!", " ", ""],#分段依据
+    length_function=len   #字符统计的一句，（函数）
+)
+
+split_document = splitter.split_documents(txt_document)
+print(split_document)
+print(len(split_document))
+
